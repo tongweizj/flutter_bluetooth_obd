2c9c42f6916180e64af9bb0f3d4d2d48593f1bff
Max <tongweizj@gmail.com>
2021-06-15 13:43:03 -0400

    update

--- android/src/main/java/com/yoese/bluetooth_obd/BluetoothObdPlugin.java
+++ android/src/main/java/com/yoese/bluetooth_obd/BluetoothObdPlugin.java
@@ -92,20 +92,20 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
         public void onReceive(Context context, Intent intent) {
           obdIntent = intent;
           obdAction = intent.getAction();
-          System.out.println("Hello, World 2");
+          // System.out.println("Hello, World 2");
           if (obdAction.equals(ACTION_CONNECTION_STATUS_MSG)) {
               String connectionStatusMsg = intent.getStringExtra(ObdReaderService.INTENT_EXTRA_DATA);
               //OBD connected  do what want after OBD connection
-              System.out.println("Hello, World 3");
+              // System.out.println("Hello, World 3");
               System.out.println(context.getString(R.string.connected_ok));
               if (connectionStatusMsg.equals(context.getString(R.string.connected_ok))) {
                   //OBD connected  do what want after OBD connection
-                  System.out.println("蓝牙连接成功");
+                  // System.out.println("蓝牙连接成功");
                   obdMesg =  "蓝牙连接成功";
                   
               } else {
                   //OBD connected  do what want after OBD connection
-                  System.out.println("蓝牙连接不成功");
+                  // System.out.println("蓝牙连接不成功");
                   obdMesg =  "蓝牙连接不成功";
               }
           } else if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
@@ -125,7 +125,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
           }
         }
       };
-      System.out.println("Hello, World 1");
+      // System.out.println("Hello, World 1");
       context.registerReceiver(mObdReaderReceiver, intentFilter);
       //start service that keep running in background for connecting and execute command until you stop
       context.startService(new Intent(context, ObdReaderService.class));
@@ -137,7 +137,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       if (obdAction.equals(ACTION_CONNECTION_STATUS_MSG)) {
         String connectionStatusMsg = obdIntent.getStringExtra(ObdReaderService.INTENT_EXTRA_DATA);
         //OBD connected  do what want after OBD connection
-        System.out.println("Hello, World 3");
+        // System.out.println("Hello, World 3");
         System.out.println(context.getString(R.string.connected_ok));
         if (connectionStatusMsg.equals(context.getString(R.string.connected_ok))) {
             //OBD connected  do what want after OBD connection
@@ -148,8 +148,8 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
         }
       } else if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
         TripRecord tripRecord = TripRecord.getTripRecode(context);
-        System.out.println("tripRecord");
-        System.out.println(tripRecord);
+        // System.out.println("tripRecord");
+        // System.out.println(tripRecord);
         
         if (tripRecord.getmEngineCoolantTemp() != null) {
           obdEngineCoolantTemp = String.valueOf(tripRecord.getmEngineCoolantTemp()); ;//display coolant temp
@@ -159,7 +159,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       }
       result.success("" + obdEngineCoolantTemp);
     } else if (call.method.equals("getSpeed")) {
-      System.out.println("开始 getSpeed");
+      // System.out.println("开始 getSpeed");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getSpeed() != null) {
@@ -171,7 +171,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       result.success("" + obdSpeed);
 
     }else if (call.method.equals("getRPM")) {
-      System.out.println("开始 getRPM");
+      // System.out.println("开始 getRPM");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getEngineRpm() != null) {
@@ -183,7 +183,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       result.success("" + obdEngineRpm);
 
     }else if (call.method.equals("getAirIntakeTemperature")) {
-      System.out.println("开始 getAirIntakeTemperature");
+      // System.out.println("开始 getAirIntakeTemperature");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getmEngineCoolantTemp() != null) {
@@ -195,7 +195,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       result.success("" + obdEngineCoolantTemp);
 
     }else if (call.method.equals("getEngineLoad")) {
-      System.out.println("开始 getEngineLoad");
+      // System.out.println("开始 getEngineLoad");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getmEngineLoad() != null) {
@@ -207,7 +207,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       result.success("" + obdEngineLoad);
 
     }else if (call.method.equals("getModuleVoltage")) {
-      System.out.println("开始 getModuleVoltage");
+      // System.out.println("开始 getModuleVoltage");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getmControlModuleVoltage() != null) {
@@ -219,7 +219,7 @@ public class BluetoothObdPlugin implements FlutterPlugin, MethodCallHandler {
       result.success("" + obdModuleVoltage);
 
     }else if (call.method.equals("getDistanceMILOn")) {
-      System.out.println("开始 getDistanceMILOn");
+      // System.out.println("开始 getDistanceMILOn");
       if (obdAction.equals(ACTION_READ_OBD_REAL_TIME_DATA)) {
           TripRecord tripRecord = TripRecord.getTripRecode(context);
           if (tripRecord.getmDistanceTraveledMilOn() != null) {
